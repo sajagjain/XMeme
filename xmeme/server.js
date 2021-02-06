70604c34c9516ca1f2b3195e16f4145c07849b87
sajagjain <sajag.veer@gmail.com>
2021-02-07 05:07:58 +0530

    Bug Fixes around port and path

--- xmeme/server.js
+++ xmeme/server.js
@@ -4,15 +4,16 @@ require('./db/db');
 
 const bodyParser = require('body-parser');
 const memeRouter = require('./routes/meme-route');
+const path = require('path');
 const express = require('express');
 const app = express();
 
-const port = process.env.port || 8080;
+const port = process.env.PORT || 3000;
 
 app.set('view engine','ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'public')));
 
 
 //API
