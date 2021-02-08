00e365bed3e475843f174971db31b1fbb1d93a26
sajagjain <sajag.veer@gmail.com>
2021-02-08 18:05:58 +0530

    Added Run Files

--- install.sh
+++ install.sh
@@ -1,23 +1,38 @@
+#MongoDB install
+
 wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
 
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
 
 sudo apt-get update
 
 sudo apt-get install -y mongodb-org
 
-#mkdir -p data/db
+echo "mongodb-org hold" | sudo dpkg --set-selections
+echo "mongodb-org-server hold" | sudo dpkg --set-selections
+echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+echo "mongodb-org-tools hold" | sudo dpkg --set-selections
 
-#sudo systemctl unmask mongod
+ps --no-headers -o comm 1
 
 sudo systemctl daemon-reload
 
 sudo systemctl enable mongod
 
-sudo systemctl start mongodb
+sudo chown `whoami` /tmp/mongodb-27017.sock
 
-#sudo mongod -dbpath=$(pwd)/data/db --quiet
+sudo mkdir -p /data/db
 
+sudo service start mongod
+
+sudo mongod
+
+#Node Install
+sudo apt install nodejs -y
+
+sudo apt install npm -y
+
+#NPM Install
 cd xmeme
-
-npm start
+npm install
---
+++ server_run.sh
@@ -0,0 +1,6 @@
+cd xmeme
+
+export PORT=8081
+export DB_CONN_STRING=mongodb://localhost/xmeme-db?retryWrites=true
+
+npm start
