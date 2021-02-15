b98ba7fe602fe9f1ba12bd2f5cd790ec66aea933
Sajag Jain <sajag.veer@gmail.com>
2021-02-15 09:20:52 +0530

    Adding DDoS

--- xmeme/routes/meme-router.js
+++ xmeme/routes/meme-router.js
@@ -1,20 +1,38 @@
-const MemeController = require('../controller/meme-controller');
-const express = require('express');
-var Ddos = require('ddos');
-var ddos = new Ddos({burst:1, limit:3});
+const MemeController = require("../controller/meme-controller");
+const express = require("express");
+
+//Cock Block Setup
+const ExpressBrute = require("express-brute");
+const MongooseStore = require("express-brute-mongoose");
+const BruteForceSchema = require("express-brute-mongoose/dist/schema");
+const mongoose = require("mongoose");
+
+const model = mongoose.model(
+  "bruteforce",
+  new mongoose.Schema(BruteForceSchema)
+);
+const store = new MongooseStore(model);
+
+const bruteforce = new ExpressBrute(store,{
+    freeRetries: 5,
+    minWait: 60*60*1000,
+    maxWait: 60*60*1000
+});
+//END: Cock Block Setup
+
 
 const router = express.Router();
 
 //Routes for Meme Operations
 //GET ALL
-router.get("/",MemeController.get);
+router.get("/", MemeController.get);
 //GET BY ID
-router.get('/:id',MemeController.getById);
+router.get("/:id", MemeController.getById);
 //POST
-router.post('/',ddos.express,MemeController.post);
+router.post("/", bruteforce.prevent, MemeController.post);
 //PATCH
-router.patch('/:id', ddos.express,MemeController.patch);
+router.patch("/:id", bruteforce.prevent, MemeController.patch);
 //GET TOP 10
-router.get('/top10/:frame',MemeController.getTop10);
+router.get("/top10/:frame", MemeController.getTop10);
 
 module.exports = router;
