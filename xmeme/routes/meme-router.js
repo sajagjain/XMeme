237c11f8f961e9f7b3c86063ca1dfe8357b7a4b7
Sajag Jain <sajag.veer@gmail.com>
2021-02-15 08:26:50 +0530

    Revert "Cluster DDoS"
    This reverts commit a8c5ed67da5602484b65becb340dc19df8f56377.

--- xmeme/routes/meme-router.js
+++ xmeme/routes/meme-router.js
@@ -1,5 +1,8 @@
 const MemeController = require('../controller/meme-controller');
 const express = require('express');
+var Ddos = require('ddos');
+var ddos = new Ddos({burst:1, limit:3});
+
 const router = express.Router();
 
 //Routes for Meme Operations
@@ -7,6 +10,10 @@ const router = express.Router();
 router.get("/",MemeController.get);
 //GET BY ID
 router.get('/:id',MemeController.getById);
+//POST
+router.post('/',ddos.express,MemeController.post);
+//PATCH
+router.patch('/:id', ddos.express,MemeController.patch);
 //GET TOP 10
 router.get('/top10/:frame',MemeController.getTop10);
 
